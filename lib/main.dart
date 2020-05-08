a9be048e3591bffb6d668ce3970ab08c9eb0aedc
Edwin Villa <villaceniceros@gmail.com>
2020-05-08 15:20:44 -0600

    Added functionality to Dices

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,88 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() {
-  return runApp(
-    MaterialApp(
+void main() => runApp(DiceApp());
+
+class DiceApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.indigoAccent[700],
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Text('Da2'),
+          centerTitle: true,
+          backgroundColor: Colors.indigoAccent[700],
         ),
         body: DicePage(),
       ),
-    ),
-  );
+    );
+  }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  void changeBothDices() {
+    leftDiceNumber = Random().nextInt(6) + 1;
+    rightDiceNumber = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset('images/dice$leftDiceNumber.png'),
+              ),
+            ),
+            Expanded(
+              child: FlatButton(
+                onPressed: () {
+                  setState(() {
+                    rightDiceNumber = Random().nextInt(6) + 1;
+                  });
+                },
+                child: Image.asset('images/dice$rightDiceNumber.png'),
+              ),
+            )
+          ],
+        ),
+        SizedBox(
+          height: 20.0,
+        ),
+        RaisedButton(
+          onPressed: () {
+            setState(() {
+              changeBothDices();
+            });
+          },
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+          child: Text(
+            'Tirar ambos',
+            style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.indigoAccent[700],
+                fontWeight: FontWeight.bold),
+          ),
+        )
+      ],
+    );
   }
 }
